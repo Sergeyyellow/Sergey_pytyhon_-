84e801ea876b961ad587ebdfacd9fc7c3d863024
Yellow <pace621@inbox.ru>
2025-09-10 15:50:20 +0300

    Поправка

--- lesson_01/lesson_1_task_3.py
+++ lesson_01/lesson_1_task_3.py
@@ -1,4 +1,4 @@
-first_name = "Sergey"
-last_name = "Pronin"
-name = input("Как вас зовут? ")
-print(f"Вас зовут: {last_name} {first_name}") 
+first_name = input("Введите ваше имя: ")
+last_name = input("Введите вашу фамилию: ")
+
+print("Вас зовут: {} {}".format(last_name, first_name))
